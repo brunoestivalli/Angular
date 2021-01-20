e4228c5a404339105b2b5e87b4d8e0565b6a8e7f
brunoestivalli <bruno.estivalli@gmail.com>
2021-01-20 11:43:27 -0300

    Rotas

--- Projeto/blogPessoal/src/app/app-routing.module.ts
+++ Projeto/blogPessoal/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
-  {path: ' ', redirectTo:"entrar", pathMatch: 'full'},
+  {path:'', redirectTo:'entrar', pathMatch: 'full'},
   
  
   {path:'entrar', component:EntrarComponent},
